9fe5fa7711a6ee4fe17bd83d73af7f213ee1ac08
aguradev <wahmayun.wm@gmail.com>
2024-04-07 00:54:05 +0800

    get user session

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import { ThemeProvider } from "@/components/provider/theme";
 import LoginPage from "@/pages/Login";
 import ThemeToggle from "./components/elements/ThemeToggle";
+import supabase from "@/services/supabase";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [userSession, setUserSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getSessionUser() {
+      const { data } = await supabase.auth.getSession();
+
+      if (data) {
+        const { session } = data;
+        setUserSession(session);
+        console.log("before", session);
+
+        const {
+          data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+          setUserSession(session);
+        });
+
+        setTimeout(() => setLoading(false), 3000);
+
+        return () => subscription.unsubscribe();
+      }
+
+      return;
+    }
+
+    getSessionUser();
+  }, []);
+
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="ui-theme">
@@ -17,9 +48,15 @@ function App() {
             </div>
           </nav>
 
-          <section className={`max-w-[500px] mx-auto py-72 px-2`}>
+          {loading ? (
+            <div>loading...</div>
+          ) : userSession ? (
+            <section className="small-container">
+              <h1>Welcome</h1>
+            </section>
+          ) : (
             <LoginPage />
-          </section>
+          )}
         </main>
       </ThemeProvider>
     </>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,28 +14,30 @@ const LoginPage = () => {
 
   return (
     <>
-      <h1 className="mb-8 text-4xl font-bold text-center">
-        Welcome to GitChat 💬
-      </h1>
-      <div className="flex flex-col gap-y-3">
-        <Button className="flex overflow-hidden gap-x-2">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
-            className="w-[18px] h-[18px]"
-          />
-          Sign in with google
-        </Button>
-        <Button
-          className="flex overflow-hidden gap-x-2"
-          onClick={() => handleOauthSignIn("github")}
-        >
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-            className="w-[18px] h-[18px]"
-          />
-          Sign in with github
-        </Button>
-      </div>
+      <section className={`max-w-[500px] mx-auto py-72 px-2`}>
+        <h1 className="mb-8 text-4xl font-bold text-center">
+          Welcome to GitChat 💬
+        </h1>
+        <div className="flex flex-col gap-y-3">
+          <Button className="flex overflow-hidden gap-x-2">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
+              className="w-[18px] h-[18px]"
+            />
+            Sign in with google
+          </Button>
+          <Button
+            className="flex overflow-hidden gap-x-2"
+            onClick={() => handleOauthSignIn("github")}
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+              className="w-[18px] h-[18px]"
+            />
+            Sign in with github
+          </Button>
+        </div>
+      </section>
     </>
   );
 };
