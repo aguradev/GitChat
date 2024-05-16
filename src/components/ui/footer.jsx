a4d5c9d62fe12f56be13afc7e630b0d8dc72eea8
aguradev <wahmayun.wm@gmail.com>
2024-05-16 17:14:55 +0800

    feat: add footer

---
+++ src/components/ui/footer.jsx
@@ -0,0 +1,10 @@
+export default function FooterSection() {
+  return (
+    <footer className="py-8 text-center">
+      <p className="mb-4">
+        It still in Development, so you may encounter some bugs.
+      </p>
+      <p>{new Date().getFullYear()} &copy; Made by 👩‍💻 aguradev</p>
+    </footer>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { supabase } from "@/services/supabase";
 import ThemeToggle from "@/components/elements/ThemeToggle";
 import "boxicons";
 import { UseAppContext } from "@/components/context/AppContext";
+import FooterSection from "@/components/ui/footer";
 
 const LoginPage = () => {
   const supabaseConfig = supabase;
@@ -45,7 +46,7 @@ const LoginPage = () => {
 
   return (
     <>
-      <nav className="p-4">
+      <nav className="fixed top-0 left-0 w-full p-4">
         <div className="flex items-center justify-between mx-auto small-container">
           <div className="text-2xl font-bold">GitChat 💬</div>
 
@@ -55,28 +56,34 @@ const LoginPage = () => {
         </div>
       </nav>
 
-      <section className={`max-w-[500px] mx-auto py-72 px-2`}>
-        <h1 className="text-center h1">Welcome to GitChat 💬</h1>
-        <div className="flex flex-col gap-y-3">
-          <Button
-            className="flex overflow-hidden gap-x-2"
-            onClick={() => handleOauthSignIn("google")}
-          >
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
-              className="w-[18px] h-[18px]"
-            />
-            Sign in with google
-          </Button>
-          <Button
-            className="flex overflow-hidden gap-x-2"
-            onClick={() => handleOauthSignIn("github")}
-          >
-            <box-icon type="logo" name="github" color="silver"></box-icon>
-            Sign in with github
-          </Button>
-        </div>
-      </section>
+      <div className="flex flex-col h-screen">
+        <section
+          className={`max-w-[500px] mx-auto px-2 flex-1 place-content-center`}
+        >
+          <h1 className="text-center h1">Welcome to GitChat 💬</h1>
+          <div className="flex flex-col gap-y-3">
+            <Button
+              className="flex overflow-hidden gap-x-2"
+              onClick={() => handleOauthSignIn("google")}
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
+                className="w-[18px] h-[18px]"
+              />
+              Sign in with google
+            </Button>
+            <Button
+              className="flex overflow-hidden gap-x-2"
+              onClick={() => handleOauthSignIn("github")}
+            >
+              <box-icon type="logo" name="github" color="silver"></box-icon>
+              Sign in with github
+            </Button>
+          </div>
+        </section>
+
+        <FooterSection />
+      </div>
     </>
   );
 };
